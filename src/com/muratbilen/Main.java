c295db7eb480ab34169d53e4f806f5fab132ef22
MuratBilen <hasanmuratbilen@gmail.com>
2019-02-04 17:55:16 +0300

    AES,SHA 256,SHA 512,MD5,added. SHA 512 was not tested.

--- src/com/muratbilen/Main.java
+++ src/com/muratbilen/Main.java
@@ -3,7 +3,10 @@ package com.muratbilen;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 public class Main {
 
@@ -12,11 +15,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Please enter the encrytion type: ");
-
+        //todo ADD MENU
         String result= encryptAES(key,initVector,"some random text");
         System.out.println(decryptAES(key,initVector,result));
         System.out.println(Sha256Encryption("random"));
-
+        System.out.println(getMd5("randomized"));
+        //todo check if sha_512 method works properly or not
+        System.out.println(get_SHA_512("randomized","saltbae"));
     }
     public static String encryptAES(String key, String initVector, String value)
     {
@@ -65,11 +70,56 @@ public class Main {
         }
 
     }
+    public static String get_SHA_512(String passwordToHash, String salt){
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt.getBytes(StandardCharsets.UTF_8));
+            byte[] bytes = md.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++){
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
     private static String bytesToHex(byte[] bytes) {
         StringBuffer result = new StringBuffer();
         for (byte b : bytes) result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
         return result.toString();
     }
-    
+    public static String getMd5(String input)
+    {
+        try {
+
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // digest() method is called to calculate message digest
+            //  of an input digest() return array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//TODO ADD PBKDF AND HMAC-SHA1 HERE
 
 }
